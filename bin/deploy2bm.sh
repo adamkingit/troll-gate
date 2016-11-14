45742e32b690fe28ff09211b8f213d95c23b0a90
Adam King <rak@us.ibm.com>
2016-11-14 15:53:03 -0500

    Check troll-gate and restart as needed

--- bin/deploy2bm.sh
+++ bin/deploy2bm.sh
@@ -7,4 +7,15 @@
 echo "see env --------------------------------------------------"
 env
 echo "see env --------------------------------------------------"
-cd ansible && ansible-playbook cf_push.yml && cd -
+
+STATUS=$(curl -s -o /dev/null -w '%{http_code}' http://troll-gate.mybluemix.net)
+if [ $STATUS -eq 200 ]; then
+  echo "Deployment approved."
+  cd ansible && ansible-playbook cf_push.yml && cd -
+else
+  echo "Deployment not approved. $STATUS"
+  sleep 10
+  # https://api.travis-ci.org/build/175825525/restart
+  RESTART_URL="https://api.travis-ci.org/build/$TRAVIS_BUILD_ID/restart"
+  curl -X POST http://example.com/myconfigs/status -HAuthorization:"token $TRAVIS_TOKEN"
+fi
