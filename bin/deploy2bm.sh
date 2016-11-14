b17b06008324883e4abd6e33bc852430dd674b8d
Adam King <rak@us.ibm.com>
2016-11-14 14:50:15 -0500

    Try again to dump env

--- bin/deploy2bm.sh
+++ bin/deploy2bm.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 
-echo "Authenticate to BlueMix and push application"
-cf login -a api.ng.bluemix.net -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD"
-cf push
+# echo "Authenticate to BlueMix and push application"
+# cf login -a api.ng.bluemix.net -u "$BLUEMIX_USER" -p "$BLUEMIX_PASSWORD"
+# cf push
+
+echo "see env --------------------------------------------------"
+env
+echo "see env --------------------------------------------------"
+cd ansible && ansible-playbook cf_push.yml && cd -
